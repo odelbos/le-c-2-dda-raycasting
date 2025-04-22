73f33a4a247dd3de82c5ecf4b94c9f6c15131110
odelbos <od@phibox.com>
2025-04-22 14:54:09 +0200

    define 8x8 world

--- main.c
+++ main.c
@@ -5,6 +5,20 @@
 #define WINDOW_WIDTH   960
 #define WINDOW_HEIGHT  540
 
+#define WORLD_WIDTH    8
+#define WORLD_HEIGHT   8
+
+int world[WORLD_HEIGHT][WORLD_WIDTH] = {
+    {1, 0, 0, 0, 0, 0, 2, 3},
+    {0, 0, 1, 2, 1, 0, 0, 1},
+    {0, 0, 0, 0, 1, 0, 0, 1},
+    {0, 0, 0, 0, 3, 0, 0, 3},
+    {0, 0, 0, 2, 1, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0},
+    {1, 1, 0, 0, 0, 0, 0, 0},
+    {0, 0, 0, 0, 0, 0, 0, 0}
+};
+
 int main(void)
 {
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Raycasting in C");
