60399b540ce525c451b108e0b681dc171cb92ca1
odelbos <od@phibox.com>
2025-04-22 14:52:07 +0200

    basic raylib example

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
-#include <stdio.h>
+#include <raylib.h>
+
+#define BACKGROUND CLITERAL(Color){20, 20, 20, 255}
+
+#define WINDOW_WIDTH   960
+#define WINDOW_HEIGHT  540
 
 int main(void)
 {
-    printf("hello world\n");
-    return 0;
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Raycasting in C");
+    SetTargetFPS(60);
+
+    while ( ! WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(BACKGROUND);
+        DrawRectangle(100, 100, 50, 50, RED);
+        EndDrawing();
+    }
+
+  CloseWindow();
+  return 0;
 }
--- make.sh
+++ make.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
+MAC_OSX=false
+[ `uname` = "Darwin"  ] && MAC_OSX=true
+
 CC=gcc
 CFLAGS="-Wall -Wextra"
+
+RAYLIB_FLAGS="-I/opt/raylib-5.5/include"
+RAYLIB_LIBS="-L/opt/raylib-5.5/lib -lraylib"
+
+if $MAC_OSX; then
+  RAYLIB_FLAGS=`pkg-config --cflags raylib`
+  RAYLIB_LIBS=`pkg-config --libs raylib`
+fi
+
+echo "RAYLIB flags: $RAYLIB_FLAGS"
+echo "RAYLIB libs: $RAYLIB_LIBS"
 
 echo "Compiling ..."
 
 set -xe
 
-$CC $CFLAGS -o raycast main.c
+$CC $CFLAGS $RAYLIB_FLAGS \
+  -o raycast main.c \
+  $RAYLIB_LIBS -lm -lpthread
