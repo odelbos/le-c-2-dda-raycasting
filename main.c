8ef7f2ff44a5d17a4d205e6a355ec143f99ad8cc
odelbos <od@phibox.com>
2025-04-22 15:00:32 +0200

    add some Vec2 operations (add, sub, mul)

--- main.c
+++ main.c
@@ -34,6 +34,21 @@ typedef struct {
     float x, y;
 } Vec2;
 
+Vec2 vec2_add(Vec2 a, Vec2 b)
+{
+    return (Vec2){a.x + b.x, a.y + b.y};
+}
+
+Vec2 vec2_sub(Vec2 a, Vec2 b)
+{
+    return (Vec2){a.x - b.x, a.y - b.y};
+}
+
+Vec2 vec2_mul(Vec2 a, Vec2 b)
+{
+    return (Vec2){a.x * b.x, a.y * b.y};
+}
+
 // Convert world coordinates to map coordinates
 Vec2 world_to_map(Map map, Vec2 v)
 {
